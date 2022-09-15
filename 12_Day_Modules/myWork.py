bf9709395ed0ae5bb2fabf29e46793be120101f0
Ana <101633036+group3Ana@users.noreply.github.com>
2022-09-15 18:03:54 +0100

    question 1.1 done

---
+++ 12_Day_Modules/myWork.py
@@ -0,0 +1,14 @@
+
+import random
+
+print("\n Question 1.1 \n")
+# Write a function which generates a six digit/character random_user_id.
+
+def random_user_id():
+ _6_numbers = str(random.sample(range(0,10),6))   # 6 random numbers from 0 to 10
+ _6_numbers = _6_numbers.split(",")               # remove commas
+ id= "".join((_6_numbers))                        # join where there are spaces
+
+ print(id)
+random_user_id()
+
